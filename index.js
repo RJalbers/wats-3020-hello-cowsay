3f39e30e047c38110c22d3c2bd3b9add3b400701
RJalbers <RJalbers89@gmail.com>
2020-01-14 19:52:37 -0800

    completed project

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 
 //TODO require cowsay
-
+const cowsay = require("cowsay");
 //TODO require getargs
+const getargs = require("./modules/get-args")
 
 // get first string from command line
 let textArg = getargs.getStringArg()
+
 
 // if use entered a string cowsay it with
 // default eyes and tail
--- modules/get-args/index.js
+++ modules/get-args/index.js
@@ -1,6 +1,7 @@
 // get the first string from list of args
 // if there is no string returns null
 
+
 exports.getStringArg = function(){
   let arg = process.argv.slice(2)
  if (arg.length > 0){
